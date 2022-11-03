dedb861228853320f73fbbe5762fa94755e2b810
DmtrY94 <dmitriyyolov94@gmail.com>
2022-11-03 11:13:31 +0200

    add page manegment

--- pages/about/our-management/_id.vue
+++ pages/about/our-management/_id.vue
@@ -1,3 +1,31 @@
 <template>
-    <main>page management</main>
+    <main class="page-content page-no-hero">
+        <div class="container">
+            {{getManagement}}
+        </div>
+    </main>
 </template>
+<script>
+import getManagement from '@/queries/getManagement'
+
+export default {
+  apollo: {
+    getManagement: {
+      query: getManagement,
+      prefetch: true,
+      variables() {
+        return {
+          slug: this.$route.params.id,
+          locale: this.$i18n.locale.toUpperCase(),
+        }
+      },
+      error() {
+        this.$root.error({ statusCode: 404, message: 'Error 400. Bad request' })
+      },
+      update(data) {
+        return data.getManagement.translation
+      },
+    },
+  },
+}
+</script>
